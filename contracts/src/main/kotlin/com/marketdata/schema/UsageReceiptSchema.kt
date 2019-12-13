090eb40ea4c992c7a115159c41e3920820277238
adam-houston <adam.houston@r3.com>
2019-12-13 13:20:51 +0000

    Various tweaks and tidies

---
+++ contracts/src/main/kotlin/com/marketdata/schema/UsageReceiptSchema.kt
@@ -0,0 +1,36 @@
+package com.marketdata.schema
+
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+
+object UsageSchema
+
+object UsageSchemaV1 : MappedSchema(
+        schemaFamily = UsageSchema.javaClass,
+        version = 1,
+        mappedTypes = listOf(PersistentUsage::class.java)) {
+
+    @Entity
+    @Table(name = "usage_states")
+    class PersistentUsage(
+            @Column(name = "subscriber")
+            var subscriberName: String,
+
+            @Column(name = "redistributor")
+            var redistributorName: String,
+
+            @Column(name = "date")
+            var date: String
+
+    ) : PersistentState() {
+        // Default constructor required by hibernate.
+        constructor() : this("",
+                "",
+                "")
+    }
+}
